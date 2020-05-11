c15de133282eaf20831aa7f7613e2d347317b521
MkItSimple <choanania1@gmail.com>
2020-05-11 15:31:59 +0800

    Initial commit

---
+++ app/src/main/java/com/example/mvvmnewsapp/ui/NewsActivity.kt
@@ -0,0 +1,20 @@
+package com.example.mvvmnewsapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.mvvmnewsapp.R
+import kotlinx.android.synthetic.main.activity_news.*
+
+class NewsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news)
+
+        // Setup Navigation in Kotlin best practice
+        // Back stack is also already properly manages
+        bottomNavigationView.setupWithNavController(newsNavHostFragment.findNavController())
+    }
+}

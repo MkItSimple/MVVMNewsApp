bc042fcc57c4f185ff4696ae0c8cb92b56b37d90
MkItSimple <choanania1@gmail.com>
2020-05-12 05:33:07 +0800

    09-SearchingNews

--- app/src/main/java/com/example/mvvmnewsapp/util/Constants.kt
+++ app/src/main/java/com/example/mvvmnewsapp/util/Constants.kt
@@ -4,5 +4,6 @@ class Constants {
     companion object {
         const val API_KEY = "098082f2cf3e4683be6cc9bfe2b3a86e"
         const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
     }
 }

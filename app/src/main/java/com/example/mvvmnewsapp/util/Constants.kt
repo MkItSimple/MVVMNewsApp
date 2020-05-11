50b1b3e998de5481e47b1f4f90e020944cdb5d96
MkItSimple <choanania1@gmail.com>
2020-05-11 15:51:37 +0800

    03-RetrofitSetup

---
+++ app/src/main/java/com/example/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.mvvmnewsapp.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "098082f2cf3e4683be6cc9bfe2b3a86e"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}

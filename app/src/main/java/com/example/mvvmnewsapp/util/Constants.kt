b6ce78b9db85a76bbe9647b1c9ef992178a8d976
MkItSimple <choanania1@gmail.com>
2020-05-12 07:01:07 +0800

    12-HandlingPaginationWithRetrofit

--- app/src/main/java/com/example/mvvmnewsapp/util/Constants.kt
+++ app/src/main/java/com/example/mvvmnewsapp/util/Constants.kt
@@ -5,5 +5,6 @@ class Constants {
         const val API_KEY = "098082f2cf3e4683be6cc9bfe2b3a86e"
         const val BASE_URL = "https://newsapi.org"
         const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
     }
 }
